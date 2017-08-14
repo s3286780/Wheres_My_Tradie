49a22392c7e7ea7423adec202ca7a2b21f701470
s3286780 <30459762+s3286780@users.noreply.github.com>
2017-08-14 22:16:51 +1000

    home page user/business difference

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,35 +9,39 @@
 
                 <div class="panel-body">
                     
-                    <h3>Your ads</h3>
-                    <a href='/advertisement/create' class='btn btn-primary'>Create</a>
-                    @if(count($ads) > 0)
-                        <table class="table table-striped">
-                            <tr>
-                                <th>Title</th>
-                                <th></th>
-                                
-                            </tr>
-                            @foreach($ads as $ad)
-                                <tr>
-                                    <td>{{$ad->name}}</td>
-                                    <td>
-                                                                        
-                                        {!!Form::open(['action' => ['AdvertisementController@destroy', $ad->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                        <a href='/advertisement/{{$ad->id}}' class='btn btn-success'>View</a>
-                                        <a href='/advertisement/{{$ad->id}}/edit' class='btn btn-warning'>Edit</a>   
-                                        {{Form::hidden('_method', 'DELETE')}}
-                                        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                                        {!!Form::close()!!}
-                                    </td>
-                                </tr>
-                            @endforeach
-                            
-                        </table>
-                    @else
-                        <p>You have no ads</p>
-                    @endif
 
+                    @if(Auth::user()->account == '1')
+                        <h3>Your ads</h3>
+                        <a href='/advertisement/create' class='btn btn-primary'>Create</a>
+                        @if(count($ads) > 0)
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Title</th>
+                                    <th></th>
+                                    
+                                </tr>
+                                @foreach($ads as $ad)
+                                    <tr>
+                                        <td>{{$ad->name}}</td>
+                                        <td>
+                                                                            
+                                            {!!Form::open(['action' => ['AdvertisementController@destroy', $ad->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                            <a href='/advertisement/{{$ad->id}}' class='btn btn-success'>View</a>
+                                            <a href='/advertisement/{{$ad->id}}/edit' class='btn btn-warning'>Edit</a>   
+                                            {{Form::hidden('_method', 'DELETE')}}
+                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                            {!!Form::close()!!}
+                                        </td>
+                                    </tr>
+                                @endforeach                           
+                            </table>
+                        @else
+                            <p>You have no ads</p>
+                        @endif
+
+                   @elseif (Auth::user()->account == '0')
+                        <h3>Your searches</h3>
+                   @endif
                 </div>
             </div>
         </div>
